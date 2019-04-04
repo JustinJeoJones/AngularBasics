b520c581f985b2dd01e7b163b5bd08dc9609b1bb
JustinJonesGC <Justin@grandcircus.co>
2019-04-04 19:29:43 -0400

    added more small features

--- controller.js
+++ controller.js
@@ -3,7 +3,10 @@
 function TestController() {
 const vm = this;
 vm.message = "You just built your first controller!";
+vm.count = [0,1,2,3,4];
+vm.visable = false;
 }
+
 angular
 .module("Madlib") // using getter syntax
 .controller("TestController", TestController);
